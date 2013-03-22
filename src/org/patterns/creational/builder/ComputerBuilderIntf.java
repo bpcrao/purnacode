68264b4361646de4b48a35531349ba6de4f56192
Purna <bpcrao@gmail.com>
2013-03-22 11:48:30 +0530

    Initial version

---
+++ src/org/patterns/creational/builder/ComputerBuilderIntf.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.patterns.creational.builder;
+
+/**
+ * @author Purna
+ *
+ */
+public interface ComputerBuilderIntf {
+
+	/**
+	 * 
+	 */
+	public abstract void buildBasicComputer();
+
+	public abstract void buildMouse();
+
+	public abstract void buildWebCam();
+
+	public abstract void buildSpeakers();
+
+	/**
+	 * 
+	 */
+	public abstract IComputer getComputer();
+
+}
