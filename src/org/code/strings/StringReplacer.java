dd989e3dc1e49b56c04f55bc1a5e775cb033ae9f
Purna <bpcrao@gmail.com>
2013-03-29 13:41:30 +0530

    string replacer

--- src/org/code/strings/StringReplacer.java
+++ src/org/code/strings/StringReplacer.java
@@ -5,8 +5,41 @@ package org.code.strings;
 
 /**
  * @author Purna
- *
+ * 
  */
 public class StringReplacer {
+
+	public static void main(String ar[]) {
+		replaceSpaces("Hello WO rl d", "%20");
+	}
+
+	/**
+	 * @param input
+	 * @param replaceChars
+	 */
+	private static void replaceSpaces(String input, String replaceChars) {
+		int spacesCount = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == ' ') {
+				spacesCount++;
+			}
+		}
+		System.out.println(spacesCount);
+		char newString[] = new char[input.length() + 2 * spacesCount];
+
+		for (int i = input.length() - 1, j = newString.length - 1; i >= 0; i--, j--) {
+			if (input.charAt(i) == ' ') {
+				newString[j] = '0';
+				newString[j - 1] = '2';
+				newString[j - 2] = '%';
+				j = j - 2;
+
+			} else {
+				newString[j] = input.charAt(i);
+			}
+			System.out.println(newString);
+		}
+
+	}
 
 }
