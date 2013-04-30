3a4c8bea5f9d0a5078a9b53edd409955964be62b
Purna <bpcrao@gmail.com>
2013-04-30 17:06:37 +0530

    changed the logic

---
+++ src/org/code/CandidateCode.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package org.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Purna
+ * 
+ */
+public class CandidateCode {
+
+	static class Node {
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			return "Node [value=" + value + ", next=" + next + ", group="
+					+ group + "]";
+		}
+
+		public static int currGroup = 1;
+		String value;
+		Node next;
+		int group;
+
+		Node(String value) {
+			this.value = value;
+			this.group = currGroup++;
+		}
+	};
+
+	static Map<String, Node> nodesMap = new HashMap<String, Node>();
+
+	public static String construction(String[] input1) {
+		for (int i = 0; i < input1.length; i++) {
+			String from = input1[i].split("#")[0];
+			String to = input1[i].split("#")[1];
+
+			if (nodesMap.containsKey(from) && nodesMap.containsKey(to)) {
+				if (!isReachable(to, from)) {
+					return "No";
+				}
+			}
+
+			Node fromNode;
+			if (nodesMap.containsKey(from)) {
+				fromNode = nodesMap.get(from);
+			} else {
+				fromNode = new Node(from);
+			}
+
+			Node toNode;
+			if (nodesMap.containsKey(to)) {
+				toNode = nodesMap.get(to);
+			} else {
+				toNode = new Node(to);
+			}
+
+			fromNode.next = toNode;
+			toNode.group = fromNode.group;
+
+			nodesMap.put(from, fromNode);
+			nodesMap.put(to, toNode);
+		}
+
+		return "Yes";
+	}
+
+	/**
+	 * @param to
+	 * @param from
+	 * @return
+	 */
+	private static boolean isReachable(String to, String from) {
+		Node fromN = nodesMap.get(from);
+		Node toN = nodesMap.get(to);
+		
+		while(fromN.next!=null){
+			if(fromN.next==toN){
+			return true;	
+			}
+			fromN=fromN.next;
+		}
+		return false;
+	}
+
+
+}
--- src/org/code/TinyURL.java
+++ src/org/code/TinyURL.java
@@ -1,4 +1,6 @@
 package org.code;
+
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +10,7 @@ import java.util.Map;
  */
 public class TinyURL {
 
-	private static int uniqueId = 678000;
+	private static int uniqueId = 1925;
 	private static int radix = 62;
 	private static Map<String, String> urlsMap = new HashMap<String, String>();
 	static String[] elements = { "a", "b", "c", "d", "e", "f", "g", "h", "i",
@@ -17,11 +19,29 @@ public class TinyURL {
 			"0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 			"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y",
 			"Z" };
+	static public Map<String, Integer> map = new HashMap<String, Integer>();
 
 	public static void main(String arp[]) {
 		String shortenedUrl = shortenURL("http://www.stumbleupon.com/su/28KqXE/www.funnyordie.com/slideshows/ff413570ec/awesomely-inappropriate-test-answers-from-kids/");
 		System.out.println("To redirect to original URL");
 		System.out.println(urlsMap.get(shortenedUrl));
+		for (int index = 0; index < elements.length; index++) {
+			map.put(elements[index], new Integer(index));
+		}
+		decodeURL(shortenedUrl);
+	}
+
+	/**
+	 * @param string
+	 */
+	private static void decodeURL(String string) {
+		int sum = 0;
+		for (int i = 0; i < string.length(); i++) {
+			System.out.println(map.get(string.charAt(i) + "") + " * " + " 62^"
+					+ (string.length() - i - 1));
+			sum += map.get(string.charAt(i) + "") * Math.pow(62, i);
+		}
+		System.out.println(sum);
 	}
 
 	/**
@@ -34,13 +54,15 @@ public class TinyURL {
 	 * @return
 	 */
 	private static String shortenURL(String url) {
-		int numberToEnode = uniqueId++;
+		int numberToEnode = uniqueId;
+
 		StringBuilder code = new StringBuilder();
-		while (numberToEnode > radix) {
+		while (numberToEnode > 0) {
 			code.append(elements[numberToEnode % radix]);
 			numberToEnode = numberToEnode / radix;
 		}
-		urlsMap.put("http://tinyurl.com/"+code.toString(), url);
-		return "http://tinyurl.com/" + code.toString();
+		System.out.println(code + "END");
+		urlsMap.put(code.toString(), url);
+		return code.toString();
 	}
 }
