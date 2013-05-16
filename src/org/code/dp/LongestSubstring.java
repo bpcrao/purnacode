8f2a1c137f5aed1cf53030d1ee447a19c3eb1faa
Purna Boyapati <bpcrao@gmail.com>
2013-05-16 07:16:57 +0530

    done

--- src/org/code/dp/LongestSubstring.java
+++ src/org/code/dp/LongestSubstring.java
@@ -2,13 +2,15 @@ package org.code.dp;
 
 public class LongestSubstring {
 
+    static int check[][];
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-        String hello = "CABRREER";
-        String hello2 = "ACAAABRARAEAEARA";
-
+        String hello = "CAAB";
+        String hello2 = "DCGDADB";
+        check = new int[hello.length()][hello2.length()];
         System.out.println(LSR(hello, hello2));
     }
 
@@ -16,8 +18,18 @@ public class LongestSubstring {
 
         if (hello.length() == 0 || hello2.length() == 0)
             return 0;
+        if(check[hello.length()-1][hello2.length()-1]>0){
+            return check[hello.length()-1][hello2.length()-1];
+        }
         if ((hello.charAt(hello.length() - 1) == (hello2.charAt(hello2.length() - 1)))) {
-            return LSR(hello.substring(0, hello.length() - 1), hello2.substring(0, hello2.length() - 1)) + 1;
+            int lsr= LSR(hello.substring(0, hello.length() - 1), hello2.substring(0, hello2.length() - 1)) + 1;
+            check[hello.length()-1][hello2.length()-1]++;
+            if(check[hello.length()-1][hello2.length()-1]==1){
+            //System.out.println(hello.length()-1 +" ,"+(hello2.length()-1));
+            System.out.println(hello.charAt(hello.length() - 1));
+            }
+            return lsr;
+            
         } else {
             return Math.max(LSR(hello.substring(0, hello.length() - 1), hello2.substring(0, hello2.length())), LSR(hello.substring(0, hello.length()), hello2.substring(0, hello2.length() - 1)));
         }
